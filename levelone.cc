a04e4aa2f94576a5e364e0a8ac6e0f7c0fd4e90b
jg2chen <jg2chen@uwaterloo.ca>
2023-12-01 20:06:17 -0500

    added computer

--- levelone.cc
+++ levelone.cc
@@ -1,7 +1,27 @@
 #include "levelone.h"
 
+// Idea:
+//   Generate array of all valid moves 
 void LevelOne::nextMove(Colour c, Board &b) const {
     // Calculate best move to make
+    std::vector<std::vector<int>> moves;
+
+    for (int i = 0; i < b.getGridSize(); ++i) {
+        for (int j = 0; j < b.getGridSize(); ++j) {
+            for (int k = 0; k < b.getGridSize(); ++k) {
+                for (int l = 0; l < b.getGridSize(); ++l) {
+                    if  (b.getBoard()[i][j]->getColour() == c && b.getBoard()[i][j]->isMoveValid(k, l, b)) {
+                        std::vector<int> m = {i,j,k,l};
+                        moves.emplace_back(m);
+                    }
+                }
+            }
+        }
+    }
+
+    int random = std::rand() % moves.size();
+
+    b.makeMove(moves[random][0], moves[random][1], moves[random][2], moves[random][3], c);
 
     // Call Board's make move
 }
